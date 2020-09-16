f8179aae188475c9863c7c35a429a862c615c67d
heinop <pekka.heino@unikie.com>
2020-09-16 12:50:00 +0300

    Added middleware

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method);
+  console.log('Path:', req.path);
+  console.log('Body:', req.body);
+  console.log('---');
+  next();
+};
+app.use(requestLogger);
+
 let notes = [
   {
     id: 1,
@@ -76,6 +85,11 @@ app.post('/api/notes', (req, res) => {
 
   res.json(note);
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'});
+};
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
