dfca6312408915324d49959a4dc6a0519cb8263e
heinop <pekka.heino@unikie.com>
2020-09-16 15:15:02 +0300

    Added ui to backend

---
+++ build/precache-manifest.04e1cd1e7760dc47b2e51e3d4bcbb6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cdf6e9883c31ca876eb6e139481872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a99c4c98b113a8217c5d",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "585cd1283eed003fe59e",
+    "url": "/static/js/2.bec695f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bec695f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99c4c98b113a8217c5d",
+    "url": "/static/js/main.3f7e979d.chunk.js"
+  },
+  {
+    "revision": "49a729cd9e63a8d8aecd",
+    "url": "/static/js/runtime-main.afbfdf4a.js"
+  }
+]);
