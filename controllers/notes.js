619638cef97d4b89cc2c21b3da989bab95fbfa17
heinop <pekka.heino@unikie.com>
2020-10-18 12:42:51 +0300

    Part 4d: Added JWT authentication

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken');
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 // Get all notes
 notesRouter.get('/', async (request, response) => {
@@ -25,7 +34,12 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'invalid or missing token' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
